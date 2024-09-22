9bb4be0290e6a3a6d726c3ce4ee1fcd604fc0d1a
Danil Falaleev <danilkafalaleev@gmail.com>
2024-09-22 12:44:19 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./assets/css/*.{css}",
+		"./components/**/*.{js,vue,ts}",
+		"./layouts/**/*.vue",
+		"./pages/**/*.vue",
+		"./plugins/**/*.{js,ts}",
+		"./app.vue",
+		"./error.vue",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary_red: "#D90E32",
+				primary_darkred: "#A60933",
+				primary_pink: "#DE304E",
+				primary_dark: "#282828",
+				primary_gray: "#F3F6F8",
+
+				font_black: "#293148",
+
+				phiolet: "#7895FF",
+				light_phiolet: "#C9D4FF",
+			},
+		},
+	},
+	plugins: [],
+};
