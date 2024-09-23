33248e46d1b77c8c0b4dd1aed112bec416069011
Danil Falaleev <danilkafalaleev@gmail.com>
2024-09-23 17:06:56 +0300

    prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
-		"./assets/css/*.{css}",
+		"./assets/css/*.css",
 		"./components/**/*.{js,vue,ts}",
 		"./layouts/**/*.vue",
 		"./pages/**/*.vue",
